419ccaf183cc858e4ff35d28ae862c3fdffc6464
zhhrozhh <zhangh40@msu.edu>
2019-07-23 14:50:36 +0100

    bsi

--- src/Besseli.cpp
+++ src/Besseli.cpp
@@ -1,24 +1,95 @@
 #include "Besseli.h"
-std::pair<double, double> BSI::I(std::pair<double, double>v, std::pair<double, double>x)
+void BSI::PYB(){
+	if(!BSI::stdd){
+		Py_Initialize();
+		BSI::stdd = true;
+		BSI::mname = PyUnicode_FromString("mpmath");
+		BSI::mpmath = PyImport_Import(BSI::mname);
+		BSI::besseli = PyObject_GetAttrString(BSI::mpmath,"besseli");
+	}
+}
+// std::pair<double, double> BSI::I(std::pair<double, double>v, std::pair<double, double>x)
+// {
+// 	if(!BSI::stdd){
+// 		cout<<"py not started"<<endl;
+// 	}
+// 	PyObject* V = PyComplex_FromDoubles(v.first,v.second);
+// 	PyObject* X = PyComplex_FromDoubles(x.first,x.second);
+
+// 	PyObject* args = PyTuple_Pack(2, V, X);
+// 	PyObject* res = PyObject_CallObject(BSI::besseli, args);
+
+// 	PyObject* z = PyObject_GetAttrString(res, "__complex__");
+
+// 	PyObject* rres = PyObject_CallObject(z, NULL); 
+
+// 	std::pair<double,double> r;
+
+// 	r.first = PyComplex_RealAsDouble(rres);
+// 	r.second = PyComplex_ImagAsDouble(rres);
+// 	return r;
+// }
+
+std::complex<double> BSI::I_(PyObject* V, PyObject* X)
 {
-	Py_Initialize();
-	Py_Initialize();
-	PyObject* V = PyComplex_FromDoubles(v.first,v.second);
-	PyObject* X = PyComplex_FromDoubles(x.first,x.second);
-	PyObject* mpmath = PyUnicode_FromString("mpmath");
-	PyObject* Mpmath = PyImport_Import(mpmath);
-	PyObject* besseli = PyObject_GetAttrString(Mpmath,"besseli");
 	PyObject* args = PyTuple_Pack(2, V, X);
-	PyObject* res = PyObject_CallObject(besseli, args);
+	PyObject* res = PyObject_CallObject(BSI::besseli, args);
+	PyObject* z = PyObject_GetAttrString(res, "__complex__");
+	PyObject* rres = PyObject_CallObject(z, NULL);
+	return std::complex<double>(PyComplex_RealAsDouble(rres),PyComplex_ImagAsDouble(rres));
+}
 
-	PyObject* z = PyObject_GetAttrString(res,"__complex__");
 
-	PyObject* rres = PyObject_CallObject(z, NULL); 
+std::complex<double> BSI::I(std::complex<double> v, std::complex<double> x)
+{
+	if(!BSI::stdd)
+	{
+		std::cout<<"py not started"<<std::endl;
+	}
+	PyObject* V = PyComplex_FromDoubles(std::real(v),std::imag(v));
+	PyObject* X = PyComplex_FromDoubles(std::real(x),std::imag(v));
+	return BSI::I_(V,X);
+}
 
-	std::pair<double,double> r;
+std::complex<double> BSI::I(std::complex<double> v, double x)
+{
+	if(!BSI::stdd)
+	{
+		std::cout<<"py not started"<<std::endl;
+	}
+	PyObject* V = PyComplex_FromDoubles(std::real(v),std::imag(v));
+	PyObject* X = PyComplex_FromDoubles(x,0.0);
+	return BSI::I_(V,X);	
+}
 
-	r.first = PyComplex_RealAsDouble(rres);
-	r.second = PyComplex_ImagAsDouble(rres);
-	Py_Finalize();
-	return r;
+std::complex<double> BSI::I(double v, std::complex<double> x)
+{
+	if(!BSI::stdd)
+	{
+		std::cout<<"py not started"<<std::endl;
+	}
+	PyObject* V = PyComplex_FromDoubles(v, 0.0);
+	PyObject* X = PyComplex_FromDoubles(std::real(x), std::imag(x));
+	return BSI::I_(V,X);	
+}
+
+std::complex<double> BSI::I(double v, double x)
+{
+	if(!BSI::stdd)
+	{
+		std::cout<<"py not started"<<std::endl;
+	}
+	PyObject* V = PyComplex_FromDoubles(v,0.0);
+	PyObject* X = PyComplex_FromDoubles(x,0.0);
+	return BSI::I_(V,X);
+}
+
+void PYE()
+{
+	if(BSI::stdd)
+	{
+		Py_Finalize();
+		BSI::stdd = false;
+	}
+
 }
--- src/Besseli.h
+++ src/Besseli.h
@@ -1,5 +1,18 @@
 #include <Python.h>
 #include <utility>
+#include <complex>
+#include <iostream>
 namespace BSI{
-	std::pair<double, double> I(std::pair<double, double>, std::pair<double, double>);
-}
+	bool stdd = false;
+	PyObject* mname;
+	PyObject* mpmath;
+	PyObject* besseli;
+	void PYB();
+	// std::pair<double, double> I(std::pair<double, double>, std::pair<double, double>);
+	std::complex<double> I_(PyObject*, PyObject*);
+	std::complex<double> I(std::complex<double>, std::complex<double>);
+	std::complex<double> I(std::complex<double>, double);
+	std::complex<double> I(double, std::complex<double>);
+	std::complex<double> I(double, double);
+	void PYE();
+};
