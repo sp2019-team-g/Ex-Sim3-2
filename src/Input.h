480143e7185cd9cdb4f2ad3fcc8db9ee22eefe08
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-07-29 14:58:36 +0100

    Update Input.h

--- src/Input.h
+++ src/Input.h
@@ -1,23 +1,38 @@
-
-#ifndef INPUT_EX32_H
-#define INPUT_EX32_H
-
+#include <string>
 #include "Arguments.h"
-
-#include <iostream>
-
+using namespace std
 
 class Input
 {
-	public:
-		template<class X> static void buildArg(Arguments& a)
-		{
-			PLIST xpl = X::plist();
-			for(PLIST::iterator it = xpl.begin(); it != xpl.end(); it++)
-				it -> operator()(a);
-		}
+public:
+    Input(string &);
+    ~Input();
 
+    Arguments GetParameters() const {return arguments;}
+
+
+private:
+    Arguments arguments;
+    typedef std::map<std::string, double> SD_map;
+    typedef std::map<std::string, double>::iterator SD_it;
+    typedef std::map<std::string, char> SC_map;
+    typedef std::map<std::string, char>::iterator SC_it;
+
+    SD_map sd_map_;
+    SC_map sc_map_;
+
+    std::string input_file_path_;
+
+    std::fstream i_stream_;    //Stream for input
+
+    bool Data_Fetched_;
+
+    void FetchData();
+
+    std::pair<std::string, double> FetchDoublePair();
+    std::pair<std::string, char> FetchCharPair();
+
+    void WriteMaps();   // utilities
+    void WriteSDmap();
+    void WriteSCmap();
 };
-
-
-#endif
