22f7d94091d36ff503f6bc5d6822ce6cf9b9e77b
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-07-31 09:26:22 +0100

    Update McPricingEng.cpp

--- src/McPricingEng.cpp
+++ src/McPricingEng.cpp
@@ -19,17 +19,18 @@ double McPricingEng::price()
 
 double McPricingEng::price(Arguments& paras)
 {
-	double bsize = (double)paras.g_VAL<size_t>("batch_size");
+	long bsize = (double)paras.g_VAL<size_t>("batch_size");
 
 	double T = paras.g_VAL<double>("T");
 
 	double res = 0.0;
 
-	for(double i = 0;i<bsize;i=i+1.0)
+	for(double i = 0;i < bsize; ++i)
 	{
 
 		pro_->simulate(paras);
-		res += opt_->payoff(paras)*std::exp(-T)/T;
+		res += opt_->payoff(paras);
 	}
+	res * = std::exp(-T)/double(bsize)
 	return res;
 }
