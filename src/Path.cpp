899cbe2925ae1ab099197a5cb7c436cc63aae74c
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-08-08 18:29:44 +0100

    Update Path.cpp

--- src/Path.cpp
+++ src/Path.cpp
@@ -16,6 +16,54 @@ Path::Path(Arguments& paras)
     path_ = std::vector<double>(*paras.g_PTR<std::vector<double> >("path")); 
 }
 
+double Path::arithmeticAvg()
+{
+    double ant = 0.0;
+    double cnt = Path::T_/Path::dt_;
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        ant += *it;
+    ant = ant/cnt;
+    return ant;
+}
+double Path::geometricAbg()
+{
+    double ant = 1.0;
+    double cnt = Path::T_/Path::dt_;
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        ant *= *it;
+    ant = exp(log(ant)/cnt);
+    return ant;
+}
+bool Path::break_up(double level)
+{
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        if(*it >= level)
+            return true;
+    return false;
+}
+bool Path::break_down(double level)
+{
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        if(*it < level)
+            return true;
+    return false;
+}
+double Path::max()
+{
+    double maxx = 0.0;
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        if(*it > maxx)
+            maxx = *it;
+    return maxx;
+}
+double Path::min()
+{
+    double minn = double(RAND_MAX);
+    for(std::vector<double>::iterator it = Path::path_.begin();it != Path::path_.end();it++)
+        if(*it < minn)
+            minn = *it;
+    return minn;
+}
 
 std::ostream& operator<<(std::ostream& os, Path p)
 {
