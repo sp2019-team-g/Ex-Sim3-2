3ccbc3ed43ce44b819ae77c595ebaf0b09882804
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-08-11 16:33:26 +0100

    Create ASPutOpt.cpp

---
+++ src/ASPutOpt.cpp
@@ -0,0 +1,19 @@
+#include "ASPutOpt.h"
+
+#include "Util.h"
+ASPutOpt::ASPutOpt(double T, double K) : Option(T)
+{
+	K_ = K;
+}
+
+ASPutOpt::ASPutOpt(Arguments& paras) : Option(paras)
+{
+	K_ = paras.g_VAL<double>("K");
+}
+
+double ASPutOpt::payoff(Arguments &paras)
+{
+    Path *path = new Path(paras);
+	double t = path->arithmeticAvg();
+	return UF::max(K_ - t, 0.0);
+}
