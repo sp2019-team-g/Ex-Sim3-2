3e7da68d7104aa497b2ba4fd6c5749739644ef44
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-08-11 15:37:26 +0100

    Update ASPricingEng.h

--- src/ASPricingEng.h
+++ src/ASPricingEng.h
@@ -1,1 +1,30 @@
+#ifndef MCPRICINGENG_EX32_H
+#define MCPRICINGENG_EX32_H
 
+#include "PricingEng.h"
+#include "Arguments.h"
+
+class Option;
+class Process;
+
+class McPricingEng : public PricingEng
+{
+    public:
+        McPricingEng(Option*, Process*);
+        McPricingEng(Arguments&);
+        double price();
+        double price(Arguments&);
+        static PLIST plist()
+        {
+            PLIST res;
+            res.push_back(g_ASK<size_t>("nos"));
+            res.push_back(g_ASK<double>("T"));
+            return res;
+        }
+
+    private:
+        Process* pro_;
+};
+
+
+#endif
