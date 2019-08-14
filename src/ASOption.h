e50ff3903cc0586b3c3d6df616eb96a95d502002
kkkelsey <52183486+kkkelsey@users.noreply.github.com>
2019-08-14 21:03:36 +0100

    Create ASOption.h

---
+++ src/ASOption.h
@@ -0,0 +1,37 @@
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  ASOption.h
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#ifndef ASCO_EX32_H
+#define ASCO_EX32_H
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#include "Arguments.h"
+#include "Option.h"
+
+#include <string>
+
+class ASOption : public Option
+{
+    public:
+        ASOption(double, double, std::string);
+        ASOption(Arguments&);
+        double payoff(Arguments&) override;
+        static PLIST plist()
+        {
+            PLIST res;
+            res.push_back(g_ASK<double>("K"));
+            res.push_back(g_ASK<double>("T"));
+            res.push_back(g_ASK<std::string>("CP"));
+            return res;
+        }
+    private:
+        double K_;
+        std::string cp_;//CALL PUT
+        std::string ff_;//FIXED FLOATING
+        std::string ag_;//ARITHMETIC GEOMETRIC
+};
+
+#endif
+
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+//  End
+//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
