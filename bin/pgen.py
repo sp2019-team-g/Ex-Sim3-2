c75e5ef940bfd12d5fb04ca19c01d78ce8e80081
zhhrozhh <zhangh40@msu.edu>
2019-08-15 20:25:28 +0100

    fixed pics

--- bin/pgen.py
+++ bin/pgen.py
@@ -9,17 +9,22 @@ if __name__ == "__main__":
             obv = df.columns[-1]
 
             fig = plt.figure()
-            axes = fig.add_axes([0.05,0.05,0.9,0.9])
+            axes = fig.add_axes([0.1,0.1,0.8,0.8])
             axes.plot(df[obv], df.result)
             fig.savefig("pics/" + fname[:-4]+"_Res.png")
+            fig.close()
             fig = plt.figure()
-            axes = fig.add_axes([0.05,0.05,0.9,0.9])
+            
+            axes = fig.add_axes([0.1,0.1,0.8,0.8])
             axes.plot(df[obv], df.std_err)
             fig.savefig("pics/" + fname[:-4]+"_Err.png")
+            fig.close()
             fig = plt.figure()
-            axes = fig.add_axes([0.05,0.05,0.9,0.9])
+            
+            axes = fig.add_axes([0.1,0.1,0.8,0.8])
             axes.plot(df[obv], df.time_usage)
             fig.savefig("pics/" + fname[:-4]+"_Tme.png")
+            fig.close()
         except Exception as e:
             print(e)
             pass
